52cc464ed2b77b54fc5d2988c5ddc3dbbe4a21a3
Bhawuk Pahuja <bhawuk0385.cse19@chitkara.edu.in>
2021-07-25 13:31:11 +0530

    Updates

---
+++ precache-manifest.6f01c743591fa374a61627ecc0379c06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6adc41e71efd630c41ff030e426e69ff",
+    "url": "/ChatBox/index.html"
+  },
+  {
+    "revision": "8e9d40678068f28bf141",
+    "url": "/ChatBox/static/css/main.15145799.chunk.css"
+  },
+  {
+    "revision": "171a402479899069ed73",
+    "url": "/ChatBox/static/js/2.8247e876.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/ChatBox/static/js/2.8247e876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9d40678068f28bf141",
+    "url": "/ChatBox/static/js/main.06abfa8a.chunk.js"
+  },
+  {
+    "revision": "f614d009d340b45ee8d4",
+    "url": "/ChatBox/static/js/runtime-main.9ec313ff.js"
+  }
+]);
